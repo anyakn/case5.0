d05a55216f91fc5c88dae73581944b4843c8a1ec
anyaknopova <annilik22@gmail.com>
2023-11-11 20:14:26 +0700

    .

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 Балан Каролина 2
 Шилкова Ульяна 2
 '''
+
+n = int(input('Введите количество предложений: '))
+
+k = 0
+bred = ''
+while k < n:
+    bred += word + ''
+    k = bred.count('.') + bred.count('!') + bred.count('?')
