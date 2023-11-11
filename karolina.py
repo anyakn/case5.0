a76c700e4756ce1b21fd762507ddd0f6182b7444
Ba1an <karolina_karo_balan@mail.ru>
2023-11-11 20:38:25 +0700

    3

---
+++ karolina.py
@@ -0,0 +1,11 @@
+with open('input.txt', encoding='utf8') as f_in:
+    nmb_snt = f_in.readline()
+    text = f_in.readline()
+
+words = text.split()
+
+unique = []
+for word in text:
+    if word not in unique:
+        unique.append(word)
+print('unique = ', unique)
